989c3004b8833c6993b6d02479b9d41761b75aec
DimaRomanets <130175375+DimaRomanets@users.noreply.github.com>
2024-01-14 17:18:06 +0100

    hw3

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,3 @@
-// const express = require("express");
-
-// const { validateBody } = require("../../middlewares");
-
-// const schemas = require("../../schemas/contacts");
-
-// const router = express.Router();
-
-// const ctrl = require("../../controllers/contacts");
-
-// router.get("/", ctrl.getAll);
-
-// router.get("/:id", ctrl.getById);
-
-// router.post("/", validateBody(schemas.addSchema), ctrl.add);
-
-// router.delete("/:id", ctrl.remove);
-
-// router.put("/:contactId", validateBody(schemas.addSchemaPut), ctrl.updateById);
-
-// module.exports = router;
 
 const express = require("express");
 
